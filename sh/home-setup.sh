dc998d1239bfe4b285b82a16f44c7aaa4153e442
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2015-02-21 14:52:18 +0000

    updated common setup packages

---
+++ sh/home-setup.sh
@@ -0,0 +1,8 @@
+sudo pacman -S --needed --noconfirm libreoffice
+sudo pacman -S --needed --noconfirm exfat-utils fuse-exfat dosfstools
+sudo pacman -S --needed --noconfirm firefox flashplugin
+sudo pacman -S --needed --noconfirm inkscape gimp
+sudo pacman -S --needed --noconfirm skype
+sudo pacman -S --needed --noconfirm ffmpeg-compat
+aur spotify
+sudo pacman -S --needed --noconfirm kodi
