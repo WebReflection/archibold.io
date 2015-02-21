bd346eafc46868721a9f2e85ad479acafe95b5cf
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2015-02-21 14:20:07 +0000

    added touchpad related settings

--- sh/archibold.sh
+++ sh/archibold.sh
@@ -418,15 +418,18 @@ gtk-application-prefer-dark-theme=1' >> /home/$USER/.config/gtk-3.0/settings.ini
 
   sync
 
+  sudo -u $USER echo '# new tabs, same dir
+[[ -s /etc/profile.d/vte.sh ]] && . /etc/profile.d/vte.sh' >> /home/$USER/.bashrc
+
   sudo -u $USER dbus-launch gsettings set org.gnome.desktop.background picture-uri '/usr/share/backgrounds/gnome/Sandstone.jpg'
   sudo -u $USER dbus-launch gsettings set org.gnome.desktop.screensaver picture-uri '/usr/share/backgrounds/gnome/Whispy_Tails.jpg'
   sudo -u $USER dbus-launch gsettings set org.gnome.desktop.datetime automatic-timezone true
   sudo -u $USER dbus-launch gsettings set org.gnome.desktop.interface clock-show-date true
   sudo -u $USER dbus-launch gsettings set org.gnome.desktop.background show-desktop-icons true
   sudo -u $USER dbus-launch gsettings set org.gnome.Terminal.Legacy.Settings dark-theme true
-
-  sudo -u $USER echo '# new tabs, same dir
-[[ -s /etc/profile.d/vte.sh ]] && . /etc/profile.d/vte.sh' >> /home/$USER/.bashrc
+  sudo -u $USER dbus-launch gsettings set org.gnome.settings-daemon.peripherals.touchpad disable-while-typing true
+  sudo -u $USER dbus-launch gsettings set org.gnome.settings-daemon.peripherals.touchpad natural-scroll true
+  sudo -u $USER dbus-launch gsettings set org.gnome.settings-daemon.peripherals.touchpad tap-to-click true
 
 else
   echo 'TIMEOUT 0
