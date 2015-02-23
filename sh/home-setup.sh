80f13b41c42d368a6da459295a6b29d268ea536b
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2015-02-23 14:03:58 +0000

    fixed typo in home-setup

--- sh/home-setup.sh
+++ sh/home-setup.sh
@@ -4,5 +4,5 @@ sudo pacman -S --needed --noconfirm firefox flashplugin
 sudo pacman -S --needed --noconfirm inkscape gimp
 sudo pacman -S --needed --noconfirm skype
 sudo pacman -S --needed --noconfirm ffmpeg-compat
-archiboold install spotify
+archibold install spotify
 sudo pacman -S --needed --noconfirm kodi
