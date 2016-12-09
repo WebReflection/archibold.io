b169a783bfd98b16b2b50e835e7b8e9fa008ced3
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2016-12-09 15:30:30 +0100

    help

--- js/help.js
+++
@@ -1,7 +0,0 @@
-this.onload = function () {
-  document.body.innerHTML = tinydown(
-    (document.body.textContent || document.body.innerText).replace(
-      /^[\s\S]+?echomd '([\s\S]+?)'.*$/, '$1'
-    )
-  );
-};
---
+++ js/transformer.js
@@ -0,0 +1,20 @@
+// archibold.io, by Andrea Giammarchi @WebReflection
+this.onload = function () {
+
+  // keep styles in the page
+  for (var
+    tmp = document.head || document.getElementsByTagName('head')[0],
+    el = document.getElementsByTagName('link'),
+    i = 0; i < el.length; i++
+  ) {
+    tmp.appendChild(el[i]);
+  }
+
+  // write markdown instead of text
+  document.body.innerHTML = (new showdown.Converter()).makeHtml(
+    (document.body.textContent || document.body.innerText).replace(
+      /^[\s\S]+?echomd '([\s\S]+?)'.*$/, '$1'
+    )
+  );
+
+};
