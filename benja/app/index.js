01e33b4a38c97ed04e77234353e0f78de1250ad9
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2019-10-03 12:01:09 +0200

    using express

--- benja/app/index.js
+++ benja/app/index.js
@@ -1,30 +1,21 @@
 // to have access to local or global scripts
 require(process.cwd() + '/node_modules/benja').paths();
 
-// simple server example
-require('http')
-  .createServer(require('tiny-cdn').create({}))
-  .listen(8080, '0.0.0.0');
-  // will respond to :80 too via iptables
+// basic static server
+const express = require('express');
+const server = express();
+server.use(express.static('.'));
+server.listen(8080);
 
-// simple app example
-const
-  electron = require('electron'),
-  app = electron.app,
-  BrowserWindow = electron.BrowserWindow
-;
-
-// in case by default WebGL doesn't work ... (rpi or others)
+// electron app
+const electron = require('electron');
+const {BrowserWindow, app} = electron;
 app.commandLine.appendSwitch('--ignore-gpu-blacklist');
 
-// once the app is ready
 app.once('ready', () => {
 
-  const area = electron.screen.getPrimaryDisplay().workAreaSize;
-
-  // a reference is needed so the GC
-  // won't kill the view
-  this.window = new BrowserWindow({
+  const {width, height} = electron.screen.getPrimaryDisplay().workAreaSize;
+  let window = new BrowserWindow({
     webPreferences: {
       nodeIntegration: true,
       nodeIntegrationInWorker: true,
@@ -37,18 +28,15 @@ app.once('ready', () => {
     fullscreen: true,
     x: 0,
     y: 0,
-    width: area.width,
-    height: area.height
+    width,
+    height
   });
 
-  this.window
-    .once('closed', () => {
-      // cleanup the reference
-      this.window = null;
-    })
+  window
+    .once('closed', () => { window = null; })
     .loadURL('http://localhost:8080/');
     // test CSS
- // .loadURL('https://codepen.io/bennettfeely/full/tfbCo/');
+ // .loadURL('https://bennettfeely.com/csscreatures/');
     // test WebGL
  // .loadURL('http://get.webgl.org/');
     // stress WebGL
@@ -56,7 +44,6 @@ app.once('ready', () => {
 
 
   // for debugging purpose, it might be handy to be able
-  // to reload the window simply via `touch ~/app/reload`
+  // to reload the app simply via `touch ~/app/reload`
   require('fs').watch('reload', () => app.quit());
-
 });
